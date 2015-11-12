a49b971653f077f535bcff939c47d65be5a3ec45
Tibor Kotosz <tkotosz@inviqa.com>
2015-11-12 16:47:04 +0100

    Initial commit of the dummy driver for the behat-screenshot extension

---
+++ src/Dummy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Bex\Behat\ScreenshotExtension\Driver;
+
+use Bex\Behat\ScreenshotExtension\Driver\ImageDriverInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class Dummy implements ImageDriverInterface
+{
+    /**
+     * @param  ArrayNodeDefinition $builder
+     */
+    public function configure(ArrayNodeDefinition $builder)
+    {
+        // driver configuration tree should be built here
+    }
+
+    /**
+     * @param  ContainerBuilder $container
+     * @param  array            $config
+     */
+    public function load(ContainerBuilder $container, array $config)
+    {
+        // store the given configuration here
+    }
+
+
+    /**
+     * @param string $binaryImage
+     * @param string $filename
+     *
+     * @return string URL to the image
+     */
+    public function upload($binaryImage, $filename)
+    {
+        // upload the image here and return the image url
+        return 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+    }
+}
